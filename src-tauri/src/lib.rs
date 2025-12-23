b0c4516264897d6293cb28bc34f6188323bd66eb
Pranav Raut <pranavraut033@gmail.com>
2025-12-23 14:46:35 +0100

    chore: add Tauri desktop configuration

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,19 @@
+use tauri::Manager;
+use tauri_plugin_stronghold::Builder;
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_fs::init())
+        .setup(|app| {
+            let salt_path = app
+                .path()
+                .app_local_data_dir()
+                .expect("could not resolve app local data path")
+                .join("salt.txt");
+            app.handle().plugin(Builder::with_argon2(&salt_path).build())?;
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
