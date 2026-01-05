227669d4f61c78a78b64c4d22f38a91e19d3eebf
Pranav Raut <pranavraut033@gmail.com>
2026-01-05 01:56:02 +0100

    refactor(cover-letter): update cover letter page with enhanced editor

--- src/app/cover-letter/[jobId]/page.tsx
+++ src/app/cover-letter/[jobId]/page.tsx
@@ -1,15 +1,43 @@
-import CoverLetterEditor from "@/components/CoverLetterEditor";
-import BackButton from "@/components/BackButton";
+import {
+  getCoverLetterByJobId,
+  getResumeByJobId,
+  getJobContext,
+} from "@/actions/job";
+import { getProfile } from "@/actions/profile";
+import EnhancedCoverLetterEditor from "@/components/EnhancedCoverLetterEditor";
+import { AIProvider } from "@/contexts/AIContext";
+import { EditorProvider } from "@/contexts/EditorContext";
+import { DEFAULT_CUSTOMIZATION, extractCustomization } from "@/types/resume";
 
-export default function CoverLetterEditorPage({
+export default async function CoverLetterEditorPage({
   params,
 }: {
-  params: { jobId: string };
+  params: Promise<{ jobId: string }>;
 }) {
+  const resolvedParams = await params;
+  const jobIdNum = parseInt(resolvedParams.jobId);
+
+  const [coverLetterData, resumeData, jobData] = await Promise.all([
+    getCoverLetterByJobId(jobIdNum),
+    getResumeByJobId(jobIdNum),
+    getJobContext(jobIdNum),
+  ]);
+
   return (
-    <div>
-      <BackButton />
-      <CoverLetterEditor jobId={params.jobId} />
-    </div>
+    <EditorProvider
+      values={{
+        coverLetter: coverLetterData?.contentText || "",
+        resume: resumeData?.contentJson || (await getProfile()),
+        customization: coverLetterData
+          ? extractCustomization(coverLetterData)
+          : DEFAULT_CUSTOMIZATION,
+        job: jobData,
+      }}
+      contentType="coverLetter"
+    >
+      <AIProvider>
+        <EnhancedCoverLetterEditor />
+      </AIProvider>
+    </EditorProvider>
   );
 }
