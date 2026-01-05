219e8010308e75d31c27817b77fb6da74ac5ab7a
Pranav Raut <pranavraut033@gmail.com>
2026-01-05 01:55:56 +0100

    refactor(resume): update resume page with enhanced editor

--- src/app/resume/[jobId]/page.tsx
+++ src/app/resume/[jobId]/page.tsx
@@ -1,18 +1,11 @@
 import EnhancedResumeEditor from "@/components/EnhancedResumeEditor";
-import BackButton from "@/components/BackButton";
 
 export default async function ResumeEditorPage({
   params: _p,
 }: {
   params: Promise<{ jobId: string }>;
 }) {
-  const params = await _p
+  const params = await _p;
 
-
-  return (
-    <div>
-      <BackButton />
-      <EnhancedResumeEditor jobId={params.jobId} />
-    </div>
-  );
+  return <EnhancedResumeEditor jobId={params.jobId} />;
 }
