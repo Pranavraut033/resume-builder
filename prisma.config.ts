6a882e729ae717b8d80739cf2655ae1e6e9bdd58
Pranav Raut <pranavraut033@gmail.com>
2026-01-05 01:56:46 +0100

    chore(db): update database schema and configuration

---
+++ prisma.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig, env } from "prisma/config";
+import "dotenv/config";     // loads .env
+
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  datasource: {
+    url: env("DATABASE_URL"),
+  },
+});
