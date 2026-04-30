6d9e082691ec2ebffd34dd34a97ed5552324c31f
Pranav Raut <pranavraut033@gmail.com>
2026-04-30 23:27:17 +0200

    build: update dependencies and project config

--- next.config.ts
+++ next.config.ts
@@ -7,8 +7,8 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'logo.clearbit.com',
+        protocol: "https",
+        hostname: "logo.clearbit.com",
       },
     ],
   },
