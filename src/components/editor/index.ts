ad1038968cb470ca92bbf88210c985f509929c37
Pranav Raut <pranavraut033@gmail.com>
2026-05-25 23:27:25 +0200

    chore: remove deprecated editor, template, and ATS components

--- src/components/editor/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Shared Editor Components
- * Export all reusable editor components for Resume and Cover Letter editors
- */
-
-export { EditorLayout } from "./EditorLayout";
-export { EditorSidePanel, COLOR_PRESETS } from "./EditorSidePanel";
